ba98b9009f986a6134449861915e5b7c66f8d8d2
Abdelhak Marouane <33136280+amarouane-ABDELHAK@users.noreply.github.com>
2025-06-04 09:58:06 -0500

    Update creds-installer.py

--- creds-installer.py
+++ creds-installer.py
@@ -74,7 +74,7 @@ def fetch_new_credentials():
 
         return credentials
     except requests.RequestException as e:
-        print({"error": f"Failed to fetch credentials: {str(e)}"})
+        print({"error": "Failed to fetch credentials:"})
         exit(1)
 
 if __name__ == "__main__":
